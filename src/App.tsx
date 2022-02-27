767e6fb21d02697e6629f66ba9a32e56428d08e9
intisar eusha <iftikharrasha@gmail.com>
2022-02-27 22:09:31 +0600

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
+import { useCallback } from 'react';
 import './App.css';
 
 function App() {
+  const newTodoRef = useRef<HTMLInputElement>(null);
+  const handleToDo = useCallback(() => {
+    // if (newTodoRef.current) {
+    //   dispatch({
+    //     type: "ADD",
+    //     text: newTodoRef.current.value,
+    //   });
+    //   newTodoRef.current.value = "";
+    // }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="mb-10 text-3xl">To do App</h1>
+        <div className="flex justify-center">
+          <input className="w-4/6 text-blue-500 mx-3 py-1 px-2" type="text" ref={newTodoRef} />
+          <button className="bg-white w-2/6 rounded text-gray-500 font-bold px-5 text-xl" onClick={handleToDo}>ADD</button>
+        </div>
     </div>
   );
 }
